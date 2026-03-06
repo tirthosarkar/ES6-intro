ebd3635532ef0acb3925d6366f360c1aa2901c50
tirthosarkar <tirthosarkar1@gmail.com>
2026-03-06 18:35:54 +0600

    difference between var let const

--- let-conts.js
+++ let-conts.js
@@ -2,3 +2,22 @@ const name = 'Bangladesh';
 // name = 'Singapore';
 const countryname = 'BD-' + name;
 console.log(countryname);
+
+let price = 120;
+price = 150;
+console.log(price);
+
+// let name22 = 'Bangladesh';
+
+const drees = ['Shirt', 'Pant', 'Shoe'];
+drees.push('belt');
+console.log(drees);
+
+
+const student = {
+    name: 'Sabbir',
+    age: 22
+}
+student.name = 'Sabbir Hossain';
+student.age = 23;
+console.log(student);
