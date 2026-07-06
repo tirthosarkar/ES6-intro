43bb7190b31d6054e165e3be83ad7f33b072bca1
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-06 19:40:04 +0600

    Added GetCardHTML

--- template-string.js
+++ template-string.js
@@ -3,3 +3,17 @@ const lastName = 'Doe';
 const fullName = `My Name is ${firstName} ${lastName}`;
 const giveMe = `give me money ${10 + 20 + 30}`;
 console.log(giveMe);
+
+
+function getCardHTML(name, description, price) {
+  const div = `
+  <div class="card">
+    <h2>${name}</h2>
+    <p>price:$ ${price}</p>
+    <p>${description}</p>
+  </div>
+  `;
+  console.log(div);
+}
+
+getCardHTML('Iphone 14', 'This is a new phone', 1000);
