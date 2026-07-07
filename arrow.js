419c53ebde32253de2fab45efdaf5ff1753a80c5
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-07 14:15:35 +0600

    Added function declaration,arrow full

--- arrow.js
+++ arrow.js
@@ -14,3 +14,23 @@ const add2 =function (num1, num2) {
 //arrow function
 const add3 = (num1, num2) => num1 + num2;
 console.log(add3(50, 60));
+
+const multiply = (num1, num2) => num1 * num2;
+
+const tenTimes = (num) => num * 10;
+const tenTimes2 = num => num * 10;
+const getpi = () => 3.14;
+const addAll = (num1, num2, num3) => num1 + num2 + num3;
+
+const doMath = (x, y) => {
+  const sum = x + y;
+  const diff = x - y;
+  const result = sum * diff;
+  return result;
+}
+
+const divide = (x, y) => x / y;
+
+document.getElementById('btn').addEventListener('click', () => { })
+
+document.getElementById('btn').addEventListener('click', (event) => {});
