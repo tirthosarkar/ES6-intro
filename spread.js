00ed8c2c368c29b93eb8e118411b94d3ed63c81b
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-07 15:12:04 +0600

    Added spread.js arr all done

--- spread.js
+++ spread.js
@@ -13,8 +13,21 @@ function sum(x, y, z) {
 const result = sum(...params)
 // console.log(result);
 
+// const arr1 = [1, 2, 3];
+// const arr2 = arr1;
+// arr2.push(4);
+// console.log(arr1)
+// console.log(arr2);
+
 const arr1 = [1, 2, 3];
-const arr2 = arr1;
-arr2.push(4);
-console.log(arr1)
-console.log(arr2);
+const arr2 = [11, ...arr1, 4, 5, 6];
+// arr2.push(4);
+// console.log(arr1)
+// console.log(arr2);
+
+const person = { name: 'John', age: 30 };
+// const person2 = person;
+const person2 = { ...person , designation: 'Developer' };
+person.salary = 50000;
+console.log(person);
+console.log(person2);
