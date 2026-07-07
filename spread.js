817d5bf4a450322851980ec12fb61cdfe0cfedef
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-07 15:05:26 +0600

    Added spread.js arr push

--- spread.js
+++ spread.js
@@ -1,3 +1,20 @@
-const max = Math.max(3, 5, 7, 2, 8 );
-console.log(max);
+const max = Math.max(3, 5, 7, 2, 58 );
+// console.log(max);
 
+const numbers = [3, 5, 7, 2, 58];
+const max2 = Math.max(...numbers);
+// console.log(...numbers);
+
+const params = [3, 5, 7, 2, 58];
+function sum(x, y, z) {
+  console.log(x, y, z);
+  return x + y + z;
+}
+const result = sum(...params)
+// console.log(result);
+
+const arr1 = [1, 2, 3];
+const arr2 = arr1;
+arr2.push(4);
+console.log(arr1)
+console.log(arr2);
