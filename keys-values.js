20d6407ed2fd5511734727f92a3b1daf618f421d
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-09 02:14:37 +0600

    Added keys-value.js const keys const values const entries

--- keys-values.js
+++ keys-values.js
@@ -1,1 +1,7 @@
-const king = {name: 'King', age: 50, title: 'The Great'};
+const king = { name: 'King', age: 50, title: 'The Great' };
+const keys = Object.keys(king);
+const values = Object.values(king);
+const entries = Object.entries(king);
+console.log(keys);
+console.log(values);
+console.log(entries);
