63ee9c4e129debe19c07a0550ea80b223ff43f82
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-09 02:07:55 +0600

    Added destructuring.js const [first, second, third]

--- destructuring.js
+++ destructuring.js
@@ -13,6 +13,8 @@
 // const name = product.name;
 
 // const product = {name : "iPhone", price: 1200, brand: "Apple"};
-const { name , price} = { name: "iPhone", price: 1200, brand: "Apple" };
-console.log(name);
-console.log(price);
+const { name , price: phonePrice, camera = '8MP'} = { name: "iPhone", price: 1200, brand: "Apple" , camera: "12MP"};
+// console.log(name, phonePrice, camera);
+
+const [first, second, third] = [1, 2, 3, 4, 5];
+console.log(first, second, third);
