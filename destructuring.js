ecafe2bf38021ed33edb2ecda3b93f4f3be5f8e7
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-09 01:04:15 +0600

    Added destructuring.js product.price

--- destructuring.js
+++ destructuring.js
@@ -3,3 +3,11 @@ const product = {
   price: 1200, 
   description: "A high-performance laptop"
 };
+
+//const name = product.name;
+
+const newPrice = product.price + 100;
+const phoneName = `this is ${product.name} and the price is ${newPrice}`;
+
+const price = product.price;
+const name = product.name;
