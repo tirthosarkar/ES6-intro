951b6c4966d1a5c6912f76a17cb55f985dcba195
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-09 01:59:38 +0600

    Added destructuring.js product.price const { name , price}

--- destructuring.js
+++ destructuring.js
@@ -1,13 +1,18 @@
-const product = {
-  name: "Laptop",
-  price: 1200, 
-  description: "A high-performance laptop"
-};
+// const product = {
+//   name: "Laptop",
+//   price: 1200,
+//   description: "A high-performance laptop"
+// };
 
-//const name = product.name;
+// //const name = product.name;
 
-const newPrice = product.price + 100;
-const phoneName = `this is ${product.name} and the price is ${newPrice}`;
+// const newPrice = product.price + 100;
+// const phoneName = `this is ${product.name} and the price is ${newPrice}`;
 
-const price = product.price;
-const name = product.name;
+// const price = product.price;
+// const name = product.name;
+
+// const product = {name : "iPhone", price: 1200, brand: "Apple"};
+const { name , price} = { name: "iPhone", price: 1200, brand: "Apple" };
+console.log(name);
+console.log(price);
