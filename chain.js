b3b40373bce14b8142320e333a7a73f283a88bb9
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-09 23:54:57 +0600

    Added chain.js

---
+++ chain.js
@@ -0,0 +1,16 @@
+const employee = {
+  name: "John Doe",
+  1: 'desk one',
+  "home address": "123 Main St",
+  salary: 50000,
+  family: {
+    father: "John Doe Sr.",
+    spouse: "Jane Doe",
+    children: {
+      name: 'jane doe'
+      age: 10
+    }
+  },
+  position: "Software Engineer"
+};
+
