e5dc1dbede6a174b74ac54518976bc6b93cb8af9
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-09 23:36:47 +0600

    Added dot-bracket.js

---
+++ dot-bracket.js
@@ -0,0 +1,14 @@
+const employee = {
+  name: "John Doe",
+  1: 'desk one',
+  "home-address": "123 Main St",
+  position: "Software Engineer"
+};  
+
+//dot bracket notation
+
+
+console.log(employee["name"]); // Output: John Doe  
+console.log(employee["position"]); // Output: Software Engineer
+console.log(employee["1"]); // Output: desk one
+console.log(employee["home-address"]); // Output: 123 Main St
