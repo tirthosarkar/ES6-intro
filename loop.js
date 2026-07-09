4e78291eee938f231c726fcc6aae9d8f16f564e1
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-10 01:20:56 +0600

    Added loop.js const employees

--- loop.js
+++ loop.js
@@ -1,5 +1,13 @@
 const number = [1, 2, 3, 4, 5];
 
-for (const num of number) {
-    console.log(num);
+// for (const num of number) {
+//     console.log(num);
+// }
+
+const employees = {
+  name: "John",
+  1: "Manager",
+  "home address": "123 Main St",
+  salary: 50000,
+  position: "Software Engineer",
 }
